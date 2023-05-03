d57cd78fab3792aed4b4d2ac44d2ac604e832183
matth <matthieu.dagommer@gmail.com>
2023-05-03 04:38:14 -0400

    added metrics

--- metrics.py
+++ metrics.py
@@ -41,7 +41,54 @@ def SNR(y1,y2):
 def SNR_improvement(y_in, y_out, y_clean):
     return SNR(y_clean, y_out)-SNR(y_clean, y_in)
     
-    
+""" Source Code:
+@inproceedings{agarla2021spectralmeasures,
+ author = {Agarla, Mirko and Bianco, Simone and Celona, Luigi and Schettini, Raimondo and Tchobanou, Mikhail},
+ year = {2021},
+ title = {An analysis of spectral similarity measures},
+ organization = {Society for Imaging Science and Technology},
+ booktitle = {Color and Imaging Conference},
+ volume = {2021},
+ number = {6},
+ doi = {https://doi.org/10.2352/issn.2169-2629.2021.29.300},
+ pages = {300--305},
+}
+"""
+def computeGFC(groundTruth, recovered):
+    """
+    Compute Goodness-of-Fit Coefficient (GFC) between the recovered and the
+    corresponding ground-truth image
+    :param groundTruth: ground truth reference image.
+        numpy.ndarray (Height x Width x Spectral_Dimension)
+    :param recovered: image under evaluation.
+        numpy.ndarray (Height x Width x Spectral_Dimension)
+    Returns:
+        GFC between `recovered` and `groundTruth`
+    """
+    assert groundTruth.shape == recovered.shape, \
+        "Size not match for groundtruth and recovered spectral images"
+    #Slight modification made
+    groundTruth = torch.clip(groundTruth.view(-1).float(), 0, 1)
+    recovered = torch.clip(recovered.view(-1).float(), 0, 1)
+
+    GFCn = torch.sum(torch.multiply(groundTruth, recovered))
+
+    GFCd = torch.multiply(torch.sqrt(torch.sum(torch.pow(groundTruth, 2))),
+                       torch.sqrt(torch.sum(torch.pow(recovered, 2))))
+
+    GFC = torch.divide(GFCn, GFCd)
+
+    return torch.mean(GFC).cpu().detach().item()
+
+
+def PSNR(original, compressed):
+    mse = torch.mean((original.view(-1).float() - compressed.view(-1).float()) ** 2)
+    if (mse == 0):  # MSE is zero means no noise is present in the signal .
+        return 100  # Therefore PSNR have no importance.
+
+    psnr = 20 * torch.log10(torch.max(original.view(-1).float()) / torch.sqrt(mse))
+
+    return psnr.cpu().detach().item()  # db
     
     
     
